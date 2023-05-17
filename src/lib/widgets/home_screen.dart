7906e3dfa1f34a4a6f57f3ddd731eab7b052aaff
xuantruongit32 <xuantruongit32@gmail.com>
2023-05-17 20:13:10 +0700

    Update home_screen

--- src/lib/widgets/home_screen.dart
+++ src/lib/widgets/home_screen.dart
@@ -1,30 +1,76 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'signin.dart';
 import 'package:flutter/material.dart';
+import 'signin.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatelessWidget {
+  void _showAddOptions(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: Icon(Icons.add),
+                title: Text('Add Channel'),
+                onTap: () {
+                  // Handle Add Channel option
+                  Navigator.pop(context); // Close the bottom sheet
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.add),
+                title: Text('Add Category'),
+                onTap: () {
+                  // Handle Add Category option
+                  Navigator.pop(context); // Close the bottom sheet
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
+  void _handleLogout(BuildContext context) {
+    // Handle Logout option
+    // You can perform any necessary logout logic here
+    Navigator.pop(context); // Close the drawer
+  }
 
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Logout"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut().then((value) {
+      appBar: AppBar(
+        title: Text('CleanTiktok'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () {
+                  FirebaseAuth.instance.signOut().then((value) {
               print("Signed Out");
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => SignInScreen()));
             });
-          },
+              },
+            ),
+          ],
         ),
+      ),
+      body: Center(
+        child: Text('Home Screen'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _showAddOptions(context),
       ),
     );
   }
 }
+
