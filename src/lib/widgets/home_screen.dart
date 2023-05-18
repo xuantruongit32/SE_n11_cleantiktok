7c801275249a4afcaaebaa5fc28846096f6e7eac
xuantruongit32 <xuantruongit32@gmail.com>
2023-05-18 21:25:15 +0700

    Update home_screen

--- src/lib/widgets/home_screen.dart
+++ src/lib/widgets/home_screen.dart
@@ -13,46 +13,43 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<String> categories = [];
-  List<Channel> channels = []; 
+  List<Channel> channels = [];
 
   Future<void> fetchData() async {
-  final userId = FirebaseAuth.instance.currentUser?.uid;
+    final userId = FirebaseAuth.instance.currentUser?.uid;
 
-  if (userId != null) {
-    DocumentSnapshot userSnapshot =
-        await FirebaseFirestore.instance.collection('users').doc(userId).get();
-    if (userSnapshot.exists) {
+    if (userId != null) {
+      DocumentSnapshot userSnapshot =
+          await FirebaseFirestore.instance.collection('users').doc(userId).get();
+      if (userSnapshot.exists) {
         List<String> fetchedCategories =
             (userSnapshot.data() as Map<String, dynamic>?)?['categories']
-                    ?.cast<String>()
-                    ?.toList() ??
-                [];
+                ?.cast<String>()
+                ?.toList() ??
+            [];
 
-    if (userSnapshot.exists) {
-      List<Map<String, dynamic>> fetchedChannels =
-          (userSnapshot.data() as Map<String, dynamic>?)?['channels']
-              ?.cast<Map<String, dynamic>>()
-              ?.toList() ??
-              [];
-      List<Channel> fetchedData = []; // List to store fetched Channel instances
-      // Create Channel instances and add them to the fetchedData list
-      for (var channelData in fetchedChannels) {
-        String category = channelData['category'] ?? '';
-        String channelUid = channelData['channelUid'] ?? '';
-        Channel channel = Channel(category: category, url: channelUid);
-        fetchedData.add(channel);
+        if (userSnapshot.exists) {
+          List<Map<String, dynamic>> fetchedChannels =
+              (userSnapshot.data() as Map<String, dynamic>?)?['channels']
+                  ?.cast<Map<String, dynamic>>()
+                  ?.toList() ??
+                  [];
+          List<Channel> fetchedData = [];
+          for (var channelData in fetchedChannels) {
+            String category = channelData['category'] ?? '';
+            String channelUid = channelData['channelUid'] ?? '';
+            Channel channel = Channel(category: category, url: channelUid);
+            fetchedData.add(channel);
+          }
+
+          setState(() {
+            channels = fetchedData;
+            categories = fetchedCategories;
+          });
+        }
       }
-
-      setState(() {
-        channels = fetchedData; // Update the channels list with fetchedData
-        categories = fetchedCategories;
-      });
     }
   }
-}
-
-                  
-
 
   void _showAddOptions(BuildContext context) {
     showModalBottomSheet(
@@ -66,14 +63,20 @@ class _HomeScreenState extends State<HomeScreen> {
                 leading: Icon(Icons.add),
                 title: Text('Add Channel'),
                 onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => AddChannel(categories: categories)));
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AddChannel(categories: categories)),
+                  );
                 },
               ),
               ListTile(
                 leading: Icon(Icons.add),
                 title: Text('Add Category'),
                 onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => AddCategory()));
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AddCategory()),
+                  );
                 },
               ),
               ListTile(
@@ -132,6 +135,5 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-}
 }
 
