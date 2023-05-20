520fe8ecaaf7b6924d3facc6ddee8eb261b4ec59
xuantruongit32 <xuantruongit32@gmail.com>
2023-05-20 18:39:23 +0700

    Update color

--- src/lib/widgets/reset_password.dart
+++ src/lib/widgets/reset_password.dart
@@ -30,9 +30,9 @@ class _ResetPasswordState extends State<ResetPassword> {
           height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
               gradient: LinearGradient(colors: [
-            hexStringToColor("CB2B93"),
-            hexStringToColor("9546C4"),
-            hexStringToColor("5E61F4")
+            hexStringToColor("000000"),
+            hexStringToColor("69C9D0"),
+            hexStringToColor("EE1D52")
           ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
           child: SingleChildScrollView(
               child: Padding(
